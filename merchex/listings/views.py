e606eb6960c476a4e4981e5413f73663ffe57864
Abdrahim-SAWADOGO <sawadogoabdrahim2001@gmail.com>
2025-10-16 09:22:03 +0000

    mdf

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -4,15 +4,7 @@ from .models import Band
 
 def hello(request):
     bands=Band.objects.all()
-    return HttpResponse(f"""
-        <h1>Page d'accueil!</h1>
-        <p>Mes groupes préférés sont :<p>
-        <ul>
-            <li>{bands[0].name}</li>
-            <li>{bands[1].name}</li>
-            <li>{bands[2].name}</li>
-        </ul>
-    """)
+    return render(request, 'listings/hello.html')
 
 def about(request):
     return HttpResponse("Hello, world. You're at the about index.")
