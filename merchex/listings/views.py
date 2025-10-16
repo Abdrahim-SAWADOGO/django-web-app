8e8cb689e30b9e60d3134a50d5f794a178b6821c
Abdrahim-SAWADOGO <sawadogoabdrahim2001@gmail.com>
2025-10-16 09:37:53 +0000

    mdf

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -4,7 +4,8 @@ from .models import Band
 
 def hello(request):
     bands=Band.objects.all()
-    return render(request, 'listings/hello.html')
+    return render(request, 'listings/hello.html',
+                  {'bands': bands})
 
 def about(request):
     return HttpResponse("Hello, world. You're at the about index.")
