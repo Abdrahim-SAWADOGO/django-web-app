9c1e46e68309cfbe202f766bdc8ac1c8c18922d3
Abdrahim-SAWADOGO <sawadogoabdrahim2001@gmail.com>
2025-10-16 15:34:23 +0000

    mdf

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,7 +1,18 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils.timezone import now
+
+# listings/models.py
 
 class Band(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(max_length=50,default="")
+    biography = models.fields.CharField(max_length=1000,default="")
+    year_formed = models.fields.IntegerField(
+    validators=[MinValueValidator(1900), MaxValueValidator(now().year)],default=now().year
+    )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
    
 
 class Listing(models.Model):
